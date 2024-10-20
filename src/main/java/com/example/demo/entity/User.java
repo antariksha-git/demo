674f2b26899e72c3431e8e7257cd149d28e22e56
antariksha-git <heyantariksha@gmail.com>
2024-10-20 13:44:31 +0530

    Added user entity

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private long id;
+    private String name;
+    private String password;
+    private String email;
+    private String phone;
+}
